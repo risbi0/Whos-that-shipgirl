bd630cc3391b53d2ce44ce0f776c8e0a545ae1f4
risbi0 <reyestedowen123@gmail.com>
2023-04-07 23:19:04 +0800

    add check

--- main.py
+++ main.py
@@ -22,9 +22,12 @@ class Menu(discord.ui.View):
 
 	@discord.ui.button(label='Leave Game', style=discord.ButtonStyle.danger)
 	async def leave_game(self, interaction, button):
-		del record[interaction.user.id]
-		await interaction.response.edit_message(content=f"Participants:\n{''.join([f'<@{player_id}> ' for player_id in record.keys()])}")
-		await interaction.followup.send(content=f'You have left the game.', ephemeral=True)
+		if interaction.user.id in record:
+			del record[interaction.user.id]
+			await interaction.response.edit_message(content=f"Participants:\n{''.join([f'<@{player_id}> ' for player_id in record.keys()])}")
+			await interaction.followup.send(content=f'You have left the game.', ephemeral=True)
+		else:
+			await interaction.response.send_message(content=f'You have haven\'t joined the game.', ephemeral=True)
 
 	@discord.ui.button(label='Start Game', style=discord.ButtonStyle.success)
 	async def start_game(self, interaction, button):
