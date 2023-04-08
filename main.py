f9679a9d68ebc72743524a2d0df7c0559f7c15de
risbi0 <reyestedowen123@gmail.com>
2023-04-08 15:58:53 +0800

    add message on start message

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import discord, os, random, json, math
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+INFO = 'Guess the name of the shipgirl! 10 rounds and 15 second time limit.'
+
 with open('leaderboard.json', 'r') as f:
     leaderboard_data = json.load(f)
 
@@ -18,14 +20,14 @@ class Menu(discord.ui.View):
 	async def join_game(self, interaction, button):
 		# initialize player score
 		record[interaction.user.id] = 0
-		await interaction.response.edit_message(content=f"Participants:\n{''.join([f'<@{player_id}> ' for player_id in record.keys()])}")
+		await interaction.response.edit_message(content=f"{INFO}\nParticipants:\n{''.join([f'<@{player_id}> ' for player_id in record.keys()])}")
 		await interaction.followup.send(content='You have joined the game.', ephemeral=True)
 
 	@discord.ui.button(label='Leave Game', style=discord.ButtonStyle.danger)
 	async def leave_game(self, interaction, button):
 		if interaction.user.id in record:
 			del record[interaction.user.id]
-			await interaction.response.edit_message(content=f"Participants:\n{''.join([f'<@{player_id}> ' for player_id in record.keys()])}")
+			await interaction.response.edit_message(content=f"{INFO}\nParticipants:\n{''.join([f'<@{player_id}> ' for player_id in record.keys()])}")
 			await interaction.followup.send(content=f'You have left the game.', ephemeral=True)
 		else:
 			await interaction.response.send_message(content=f'You have haven\'t joined the game.', ephemeral=True)
@@ -182,7 +184,7 @@ async def start(ctx):
 
 	ship_names = SHIP_NAMES.copy()
 	record = {}
-	await ctx.send(view=Menu())
+	await ctx.send(INFO, view=Menu())
 
 @bot.command()
 async def leaderboard(ctx):
